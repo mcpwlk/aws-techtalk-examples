07cbceb8d5e4490b3caa0b11497c4afd22ca9d1f
Michał Pawluk <michal.p.pawluk@gmail.com>
2018-10-16 20:17:46 +0200

    Add EIP

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -4,3 +4,13 @@ resource "aws_instance" "nginx" {
   subnet_id              = "${aws_subnet.sub.id}"
   vpc_security_group_ids = ["${aws_security_group.external.id}"]
 }
+
+resource "aws_eip" "nginx_eip" {
+  instance = "${aws_instance.nginx.id}"
+  vpc      = true
+}
+
+resource "aws_eip_association" "nginx_eip_assoc" {
+  instance_id   = "${aws_instance.nginx.id}"
+  allocation_id = "${aws_eip.nginx_eip.id}"
+}
